2844d8468000d734f7139029c46739ab7dbc2e3e
kumarization <sunnykumar@me.com>
2014-02-12 01:29:04 -0600

    Sunny kumar
    hw5

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140212072032) do
+
+  create_table "products", force: true do |t|
+    t.string   "title"
+    t.integer  "year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
